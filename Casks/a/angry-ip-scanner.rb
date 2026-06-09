8fdbd85637ded55f0b96862ebeb46084bad40dad
tmatzner42 <tmatzner@mdigi.de>
2026-06-09 12:32:54 +0200

    Comment out macOS dependency for Angry IP Scanner
    Comment out the macOS version dependency for Angry IP Scanner.

--- Casks/a/angry-ip-scanner.rb
+++ Casks/a/angry-ip-scanner.rb
@@ -11,7 +11,7 @@ cask "angry-ip-scanner" do
   desc "Network scanner"
   homepage "https://angryip.org/"
 
-  depends_on macos: ">= :big_sur"
+  #depends_on macos: ">= :big_sur"
 
   app "Angry IP Scanner.app"
 
