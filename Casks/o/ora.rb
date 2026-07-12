987ae22498e6d67f9bceb68f490bf6930f7c5959
tmatzner42 <tmatzner@mdigi.de>
2026-07-12 05:14:55 +0000

    fix ora sha

--- Casks/o/ora.rb
+++ Casks/o/ora.rb
@@ -2,7 +2,7 @@ cask "ora" do
   arch arm: "Arm64"
 
   version "1.0.8"
-  sha256 arm:   "sha256:af68044cc9a4ffed402624d9e99057ab8fbf5e4d009fe4b16ceff303a6117ffa"
+  sha256 arm:   "af68044cc9a4ffed402624d9e99057ab8fbf5e4d009fe4b16ceff303a6117ffa"
 
   url "https://github.com/benedict2310/ora/releases/download/v#{version}/Ora-#{version}.dmg",
       verified: "https://github.com/benedict2310/ora/"
